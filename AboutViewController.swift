0e519ec8561c96b3dd76c3bf401240120cf4f592
desewenkdk <171018since400@gmail.com>
2019-03-29 22:48:59 +0900

    when you want to use webkit view, go to ProjectConfig(right click on project)-> General -> Linked Frameworks and Libraries -> add WebKit.framework

---
+++ AboutViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AboutViewController.swift
+//  basicApp_BullsEye
+//
+//  Created by 김동규 on 29/03/2019.
+//  Copyright © 2019 kdk. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closeView(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
