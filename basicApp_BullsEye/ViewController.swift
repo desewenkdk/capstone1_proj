807aa2a5eb9b352c1e53cb6bcb2039f02fffff63
desewenkdk <171018since400@gmail.com>
2019-03-29 13:22:56 +0900

    construct Stack view frame, and add alert button

--- basicApp_BullsEye/ViewController.swift
+++ basicApp_BullsEye/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBAction func startButton(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Result", message: "The value of slidebar is", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "ok", style: UIAlertAction.Style.default, handler: nil)
+        
+        alert.addAction(okAction)
+        present(alert,animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
